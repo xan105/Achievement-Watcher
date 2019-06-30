9e3ec0d8f07acea3d140e7ab510dd545b7d1f7c1
Anthony Beaumont <dev.xan105@outlook.be>
2019-06-30 13:17:28 +0700

    srv update
    Dependencies update

--- server/node_modules/feature-policy/lib/makepolicy.js
+++
@@ -1,8 +0,0 @@
-var config = require('./config')
-
-module.exports = function makePolicy (options) {
-  return Object.keys(options.features).map(function (featureKey) {
-    const dasherizedKey = config.features[featureKey]
-    return [dasherizedKey].concat(options.features[featureKey]).join(' ')
-  }).join(';')
-}
--- server/node_modules/frameguard/lib/isstring.js
+++
@@ -1,3 +0,0 @@
-module.exports = function (val) {
-  return typeof val === 'string' || val instanceof String
-}
