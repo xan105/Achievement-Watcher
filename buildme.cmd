4140a139c47a5ccc2f75e956e2b6406bda48b95d
Anthony Beaumont <dev.xan105@outlook.be>
2020-02-24 02:07:12 +0700

    package update

--- buildme.cmd
+++ buildme.cmd
@@ -5,7 +5,6 @@ echo ================
 call "%~dp0service\buildme.cmd"
 echo Building Setup
 echo ==============
-call "%~dp0app\native\regedit\build.cmd"
 call "%~dp0setup\rcedit.cmd"
 call "%~dp0setup\mkasar.cmd"
 echo COMPILE SETUP
