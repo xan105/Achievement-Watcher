4f038c3b4049a06f973e1a819f813faf06279bfe
Anthony Beaumont <dev.xan105@outlook.be>
2020-03-29 04:28:18 +0700

    1.3.3

--- setup/rcedit.cmd
+++ setup/rcedit.cmd
@@ -1,4 +1,4 @@
 @echo off
 cd "%~dp0"
 xcopy /Y ..\app\node_modules\electron\dist\electron.exe prod\AchievementWatcher.exe 
-rcedit-x64.exe prod\AchievementWatcher.exe --set-icon %~dp0prod/icon.ico --set-file-version "1.3.2" --set-product-version "1.3.2" --set-version-string "CompanyName" "Anthony Beaumont" --set-version-string "ProductName" "Achievement Watcher" --set-version-string "FileDescription" "Achievement Watcher" --set-version-string "OriginalFilename" "AchievementWatcher.exe" --set-version-string "InternalName" "AchievementWatcher" --set-version-string "LegalCopyright" "Copyright 2019-2020 Anthony Beaumont."
+rcedit-x64.exe prod\AchievementWatcher.exe --set-icon %~dp0prod/icon.ico --set-file-version "1.3.3" --set-product-version "1.3.3" --set-version-string "CompanyName" "Anthony Beaumont" --set-version-string "ProductName" "Achievement Watcher" --set-version-string "FileDescription" "Achievement Watcher" --set-version-string "OriginalFilename" "AchievementWatcher.exe" --set-version-string "InternalName" "AchievementWatcher" --set-version-string "LegalCopyright" "Copyright 2019-2020 Anthony Beaumont."
