1bf7ac5d6a1800618f51a03c0021cf062fda975d
Anthony Beaumont <dev.xan105@outlook.be>
2020-10-25 15:03:18 +0700

    more readable

--- app/parser/sse.js
+++ app/parser/sse.js
@@ -1,52 +1,50 @@
 'use strict';
 
-module.exports.parse = (buffer) => {
-  try{
+function parse(buffer) {
+
+	if (!Buffer.isBuffer(buffer)) throw "ENOTABUFFER";
 
     const header = buffer.slice(0, 4);
-    const entryCount = parseInt(header.slice(0,1).reverse().toString('hex'),16);
+    const statsCount = toInt(header.slice(0,1));
+    const statsLength = 24; //Each entries are 24 bytes long
     
-    const data = bufferSplit( buffer.slice(header.length, buffer.length), 24);
-    if (data.length !== entryCount) throw "Unexpected file content";
+    const stats = bufferSplit(buffer.slice(header.length, buffer.length), statsLength);
+    if (stats.length !== statsCount) throw "EUNEXPECTEDFILECONTENT";
     
     let result = [];
     
-    for (let entry of data) 
+    for (let i = 0; i < stats.length; i++)
     {  
         try{
         
-          let value = parseInt(entry.slice(20,21).toString('hex'),16);
-
-          if ( value === 1 ) { //if stat has a value of 1 well then ACH_NOT_FOUND_IN_SCHEMA will be triggered
-          
+          const value = toInt(stats[i].slice(20,21));
+          if ( value === 1 )  //is an achievement or a stat when 0 or 1; is a stat 100% when > 1; NB: a stat has also an unlocktime with sse
+          {
             result.push({
-                crc: entry.slice(0,4).reverse().toString('hex'),
+                crc: toString(stats[i].slice(0,4)),
                 Achieved : value,
-                UnlockTime: parseInt(entry.slice(8, 12).reverse().toString('hex'),16)
+                UnlockTime: toInt(stats[i].slice(8, 12))
             });
-          
-          } else {
-            throw "Not an achievement but a stat";
           }
           
-        }catch(err){
-          //Do nothing
-        }
+        }catch{ /* Do nothing */ }
     }
     
     return result;
-  
-  }catch(err){
-    throw err;
-  }
 }
 
-function bufferSplit(buffer,n){
+function toString(buffer){
+	return buffer.reverse().toString('hex');
+}
 
+function toInt(buffer) {
+	return parseInt(buffer.reverse().toString('hex'),16);
+}
+
+function bufferSplit(buffer, n){
   let result = [];
-  for (let i = 0, j = 1; i < buffer.length; i += n, j++) {
-    result.push(buffer.slice(i,n*j));    
-  }
+  for (let i = 0, j = 1; i < buffer.length; i += n, j++) result.push(buffer.slice(i,n*j));
   return result;
-
 }
+
+module.exports = { parse };
