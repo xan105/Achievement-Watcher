c7cd6e4c1e6842ffd17099ebbb8fd91fee628f71
Anthony Beaumont <dev.xan105@outlook.be>
2020-10-25 15:02:58 +0700

    asar path

--- app/components/register.js
+++ app/components/register.js
@@ -1,7 +1,7 @@
 const { ipcRenderer } = require("electron");
 
 import titleBar from './titleBar/titleBar.js';
-import userAvatar from './userAvatar./index.js';
+import userAvatar from './userAvatar/index.js';
 
 customElements.define('title-bar', titleBar);
 customElements.define('user-avatar', userAvatar);
