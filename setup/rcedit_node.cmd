001b1985ba73c04c4502a2b663fb40b2ad881780
Anthony Beaumont <dev.xan105@outlook.be>
2020-10-27 10:41:59 +0700

    cosmetic

---
+++ setup/rcedit_node.cmd
@@ -0,0 +1,3 @@
+@echo off
+
+rcedit-x64.exe prod\node\node.exe --set-icon %~dp0prod/icon.ico --set-version-string "ProductName" "Achievement Watcher" --set-version-string "FileDescription" "Achievement Watcher"
