4129b04b307e0de9fc044699f3902d1bdb6961f9
Anthony Beaumont <dev.xan105@outlook.be>
2020-12-29 05:36:23 +0700

    Notification sound
    add : ps5, ps5 platinum, gog galaxy
    remove : discord
    
    NB: files have been renamed

--- app/util/toastAudio.js
+++ app/util/toastAudio.js
@@ -1,55 +1,46 @@
+'use strict';
+
 const path = require('path');
 const regedit = require('regodit');
 
 module.exports.getDefault = () => {
 
+  const _default_ = "Windows Notify System Generic.wav";
+  
   try{
   
-    let filepath = regedit.RegQueryStringValue("HKCU","AppEvents/Schemes/Apps/.Default/Notification.Default/.Current","");
+    const filepath = regedit.RegQueryStringValue("HKCU","AppEvents/Schemes/Apps/.Default/Notification.Default/.Current","");
   
     if(filepath) {
   
-      return (path.parse(filepath)).base;
+      return path.parse(filepath).base;
       
-    } else {
-      return "Windows Notify System Generic.wav";
-    }
+    } else { return _default_ }
   
-  }catch(err){
-    return "Windows Notify System Generic.wav";
-  }
-
+  }catch{ return _default_ }
 }
 
 module.exports.setCustom = (filename) => {
-
   try{
   
-    let file = path.join(process.env['WINDIR'],"Media",filename);
+    const file = path.join(process.env['WINDIR'],"Media",filename);
     
     regedit.RegWriteStringValue("HKCU","AppEvents/Schemes/Apps/.Default/Notification.Achievement/.Current","",file);
     regedit.RegWriteStringValue("HKCU","AppEvents/Schemes/Apps/.Default/Notification.Achievement/.Default","",file);
   
-  }catch(err){}
-
+  }catch{/*Do nothing*/}
 }
 
 module.exports.getCustom = () => {
-
   try{
   
-    let filepath = regedit.RegQueryStringValue("HKCU","AppEvents/Schemes/Apps/.Default/Notification.Achievement/.Current","");
+    const filepath = regedit.RegQueryStringValue("HKCU","AppEvents/Schemes/Apps/.Default/Notification.Achievement/.Current","");
   
     if(filepath) {
   
-      return (path.parse(filepath)).base;
+      return path.parse(filepath).base;
       
-    } else {
-      return "";
-    }
+    } else { return "" }
   
-  }catch(err){
-    return "";
-  }
-
+  }catch{ return "" }
 }
